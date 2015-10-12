8007d5fd3c5d12d6394317b17b3813e764021e7e
Todd Nichols <tnichols@clientresourcesinc.com>
2015-10-12 10:30:20 -0500

    create apikey constant for project service

--- src/app/projects/projects.service.js
+++ src/app/projects/projects.service.js
@@ -3,18 +3,19 @@
 
 angular
   .module('crutch')
-  .factory('Project', function($resource) {
+  .constant('apiKey', 'Token token=35bb17265317e9d61a114203ec9a1d5b')
+  .factory('Project', function($resource, apiKey) {
 
     function formatData(data) {
       return JSON.stringify({
         project: data
-      })
+      });
     }
 
     return $resource("http://localhost:3010/api/v1/projects/:id", { id: "@id" },
       {
-        'create':    { method: 'POST', transformRequest: formatData, headers: {'Authorization': 'Token token=35bb17265317e9d61a114203ec9a1d5b'} },
-        'query':   { method: 'GET', isArray: true, headers: {'Authorization': 'Token token=35bb17265317e9d61a114203ec9a1d5b'} },
+        'create':  { method: 'POST', transformRequest: formatData, headers: {'Authorization': apiKey} },
+        'query':   { method: 'GET', isArray: true, headers: {'Authorization': apiKey} },
         'show':    { method: 'GET', isArray: false },
         'update':  { method: 'PUT' },
         'destroy': { method: 'DELETE' }
