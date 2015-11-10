e585c33f8eac319dc7165c1ba0374001511a70c8
Todd Nichols <tnichols@clientresourcesinc.com>
2015-11-10 09:24:27 -0600

    add edit project feature

--- src/app/projects/projects.controller.js
+++ src/app/projects/projects.controller.js
@@ -6,6 +6,7 @@
     .controller('ProjectsController', ['Project', 'errorService', '$scope', function (Project, errorService, $scope) {
       var pc = this;
       pc.project = new Project();
+      pc.editProjectForm = {};
       pc.showAddProjectForm = false;
       pc.errors = [];
 
@@ -13,11 +14,24 @@
         pc.projects = data;
       });
 
-      pc.create = function() {
+      pc.addProject = function() {
         $.when(pc.project.$create().then(
           function() {
             pc.projects.push(pc.project);
-            pc.resetForm();
+            pc.resetAddForm();
+          },
+          function(error) {
+            pc.errors = errorService.parseErrors(error);
+          }
+        ));
+      };
+
+      pc.update = function(project) {
+        $.when(pc.project.$update(project).then(
+          function() {
+            var projectId = project.id;
+            var projectsList = $scope.projects.projects;
+            pc.resetEditForm(projectsList, projectId);
           },
           function(error) {
             pc.errors = errorService.parseErrors(error);
@@ -26,17 +40,45 @@
       };
 
       pc.delete = function(project) {
-        $.when(Project.delete(project)).then(
-          function() {
-            _.remove(pc.projects, project);
-          },
-          function(error){
-            pc.errors = errorService.parseErrors(error);
-          }
-        );
+        var confirmed = confirm('Are you sure you want to delete?');
+        if (confirmed) {
+          $.when(Project.delete(project)).then(
+            function () {
+              _.remove(pc.projects, project);
+            },
+            function (error) {
+              pc.errors = errorService.parseErrors(error);
+            }
+          );
+        }
       };
 
-      pc.resetForm = function() {
+      pc.copyProject = function(project) {
+        pc.projectCopy = angular.copy(project);
+      };
+
+      pc.cancelEdit = function(project) {
+        var projectId = project.id;
+        var projectsList = pc.projects;
+        pc.resetEditForm(projectsList, projectId, true);
+      };
+
+      pc.resetEditForm = function(projectsList, projectId, cancel) {
+        for (var i = 0; i < projectsList.length; i++) {
+          if (projectsList[i].id === projectId) {
+            projectsList[i].editProjectForm.$setPristine();
+            projectsList[i].editProjectForm.$setUntouched();
+            projectsList[i].showEditProjectForm = false;
+            if (cancel) {
+              projectsList[i] = pc.projectCopy;
+            }
+          }
+          pc.errors = [];
+          pc.project = new Project();
+        }
+      };
+
+      pc.resetAddForm = function() {
         $scope.newProjectForm.$setPristine();
         $scope.newProjectForm.$setUntouched();
         $scope.showAddProjectForm = false;
--- src/app/projects/projects.service.js
+++ src/app/projects/projects.service.js
@@ -5,23 +5,13 @@ angular
   .module('crutch')
   .factory('Project', function($resource, apiKey, baseUrl) {
 
-    function formatData(data) {
-      return JSON.stringify({
-        project: data
-      });
-    }
-
     return $resource(baseUrl + "/projects/:id",
       { id: "@id" },
       {
-        'create':  {
-          method: 'POST',
-          transformRequest: formatData,
-          headers: {'Authorization': apiKey}
-        },
+        'create':  { method: 'POST', headers: {'Authorization': apiKey} },
         'query':   { method: 'GET', isArray: true, headers: {'Authorization': apiKey} },
         'show':    { method: 'GET', isArray: false },
-        'update':  { method: 'PUT' },
+        'update':  { method: 'PUT', headers: {'Authorization': apiKey} },
         'delete':  { method: 'DELETE', headers: {'Authorization': apiKey} }
       }
     );
